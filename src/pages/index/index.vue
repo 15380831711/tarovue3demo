4ec786d2f0d198cccbdf596ada6e8713221fd5f0
15380831711 <1193494995@qq.com>
2021-11-10 12:12:34 +0800

    first commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <view class="index">
+        <view>
+            <img src="" alt="">
+        </view>
+        {{ msg }}
+        <!-- <view class="btn">
+            <nut-button type="primary" @click="handleClick('text', msg2, true)">点我</nut-button>
+        </view> -->
+        <view class="btn">
+            <nut-button type="primary" @click="toLogin()">跳转</nut-button>
+        </view>
+        <nut-toast :msg="msg" v-model:visible="show" :type="type" :cover="cover" />
+    </view>
+</template>
+
+<script>
+    import { reactive, toRefs } from "vue";
+
+    import Taro from "@tarojs/taro";
+
+    export default {
+        name: "Index",
+        components: {},
+        setup() {
+            const state = reactive({
+                msg: "欢迎使用 NutUI3.0 开发小程序",
+                msg2: "你成功了～",
+                type: "text",
+                show: false,
+                cover: false
+            });
+
+            const handleClick = (type, msg, cover = false) => {
+                state.show = true;
+                state.msg2 = msg;
+                state.type = type;
+                state.cover = cover;
+            };
+
+            const toLogin = () => {
+                Taro.navigateTo({
+                    url: "pages/login/index",
+                    success: () => {
+                        console.log("success");
+                    },
+                    fail: () => {
+                        console.log("fail");
+                    },
+                    complete: () => {
+                        console.log("complete");
+                    }
+                });
+            };
+
+            return {
+                ...toRefs(state),
+                handleClick,
+                toLogin
+            };
+        }
+    };
+</script>
+
+<style lang="scss">
+    .index {
+        font-family: "Avenir", Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+    }
+</style>
---
+++ src/pages/login/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <view class="index">
+        <view class="btn">
+            <nut-button type="primary" @click="pop()">返回</nut-button>
+        </view>
+    </view>
+</template>
+
+<script>
+    import Taro from "@tarojs/taro";
+
+    export default {
+        name: "Index",
+        components: {},
+        setup() {
+            const pop = () => {
+                Taro.navigateBack();
+            };
+            return {
+                pop
+            };
+        }
+    };
+</script>
+
+<style lang="scss">
+</style>
